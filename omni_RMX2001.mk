d52457336b87a566191ad052496babf9487ee21d
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2023-08-26 16:20:09 +0800

    Revert "RMX2001: Rebrand to RM6785"
    * From now on, I will no longer try to merge these devices
      to a single RM6785, to allow room for realme UI 3 recovery
      on RMX2151.
    
    This reverts commit 4dcd064659e542e6e2cf6a35c153b062dd053e0e.

--- omni_RMX2001.mk
+++ omni_RMX2001.mk
@@ -32,10 +32,10 @@ PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock.recovery
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := RM6785
-PRODUCT_NAME := omni_RM6785
+PRODUCT_DEVICE := RMX2001
+PRODUCT_NAME := omni_RMX2001
 PRODUCT_BRAND := realme
-PRODUCT_MODEL := RM6785
+PRODUCT_MODEL := realme 6
 PRODUCT_MANUFACTURER := realme
 PRODUCT_SHIPPING_API_LEVEL := 29
 
